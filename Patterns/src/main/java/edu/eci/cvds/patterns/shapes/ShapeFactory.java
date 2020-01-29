3f45cbf425e1514da843d70ecfbd7042c8fc530a
unknown <daniel.vargas-o@labinfo.is.escuelaing.edu.co>
2020-01-29 08:33:43 -0500

    Actualizacion

--- Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
@@ -2,21 +2,18 @@ package edu.eci.cvds.patterns.shapes;
 
 import edu.eci.cvds.patterns.shapes.concrete.*;
 
-public static class ShapeFactory{
-	public Shape create(RegularShapeType type){
+public class ShapeFactory{
+	public static Shape create(RegularShapeType type){
 		switch (type){
 			case Triangle:
 				return new Triangle();
-				break;
-			case Quadrilateral:
+		case Quadrilateral:
 				return new Quadrilateral();
-				break;
-			case Pentagon:
+		case Pentagon:
 				return new Pentagon();
-				break;
-			case Hexagon:
+		case Hexagon:
 				return new Hexagon();
-				break;
 		}
+		return null;
 	}
 }
